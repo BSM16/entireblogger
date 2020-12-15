a72294146cfd73cb386f860350672c2d6a00a3bc
BSM16 <65490985+BSM16@users.noreply.github.com>
2020-12-15 07:00:20 +0700

    Create entireload.js

---
+++ entireload.js
@@ -0,0 +1,2 @@
+$(document).ready(function() {$('.entireload').removeClass('ereplace').addClass('hidden');});
+$(document).ready(function() {$('.noThumbnail').removeClass('ehidden').addClass('block');});
